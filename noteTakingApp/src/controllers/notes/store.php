6505ce6a7ce94e87b19b63b2cd80df9eb476a593
Denver James Duran <128191960+DDuran19@users.noreply.github.com>
2024-08-30 16:47:46 +0800

    use guard clause instead

--- noteTakingApp/src/controllers/notes/store.php
+++ noteTakingApp/src/controllers/notes/store.php
@@ -18,13 +18,14 @@ $errors = [];
 if (!FormValidator::string($form['body'])) {
     $errors["body"] = "Please enter a note below";
 }
-if (count($errors) === 0) {
-    $insertedId = $db->insert("INSERT INTO notes (body, userId) VALUES (:body, :userId)", [
-        'body' => $form['body'],
-        'userId' => 1,
-    ]);
 
-    header("Location: /notes/{$insertedId}");
-    exit();
-}
-renderView("notes/create", ['heading' => $heading, 'errors' => $errors]);
+
+if (count($errors) !== 0) renderView("notes/create", ['heading' => $heading, 'errors' => $errors]);
+
+$insertedId = $db->insert("INSERT INTO notes (body, userId) VALUES (:body, :userId)", [
+    'body' => $form['body'],
+    'userId' => 1,
+]);
+
+header("Location: /notes/{$insertedId}");
+exit();
