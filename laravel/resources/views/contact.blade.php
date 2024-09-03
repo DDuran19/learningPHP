b844262398d04ece0c94185cb31001830d36619e
Denver James Duran <128191960+DDuran19@users.noreply.github.com>
2024-09-03 09:37:49 +0800

    learn about layout and slots

---
+++ laravel/resources/views/contact.blade.php
@@ -0,0 +1,8 @@
+<x-layout>
+    <x-slot:heading>
+        Contact
+    </x-slot:heading>
+    <h1>
+        Hello from the Contact
+    </h1>
+</x-layout>
---
+++ laravel/resources/views/home.blade.php
@@ -0,0 +1,8 @@
+<x-layout>
+    <x-slot:heading>
+        Homepage
+    </x-slot:heading>
+    <h1>
+        Hello from the homepage
+    </h1>
+</x-layout>
