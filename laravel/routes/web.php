8a45d089977c2b98356886a31b8034977c77cbb9
dduran19 <denverjames.duran19@outlook.com>
2024-09-04 16:01:58 +0800

    Learn Route Group

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -6,10 +6,12 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
 
-Route::get('/jobs', [JobController::class, 'index']);
-Route::post('/jobs', [JobController::class, 'store']);
-Route::get('/jobs/create', [JobController::class, 'create']);
-Route::get('/jobs/{job}', [JobController::class,'show']);
-Route::get('/jobs/{job}/edit', [JobController::class,'edit']);
-Route::patch('/jobs/{job}', [JobController::class,'update']);
-Route::delete('/jobs/{job}', [JobController::class,'destroy']);
+Route::controller(JobController::class)->group(function () {
+    Route::get('/jobs', [JobController::class, 'index']);
+    Route::post('/jobs', [JobController::class, 'store']);
+    Route::get('/jobs/create', [JobController::class, 'create']);
+    Route::get('/jobs/{job}', [JobController::class, 'show']);
+    Route::get('/jobs/{job}/edit', [JobController::class, 'edit']);
+    Route::patch('/jobs/{job}', [JobController::class, 'update']);
+    Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
+});
