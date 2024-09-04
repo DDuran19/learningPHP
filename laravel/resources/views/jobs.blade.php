dc59caadb615b9f1d4da510eac88178eb4326702
Denver James Duran <128191960+DDuran19@users.noreply.github.com>
2024-09-04 09:38:41 +0800

    quick style for each joblist

--- laravel/resources/views/jobs.blade.php
+++ laravel/resources/views/jobs.blade.php
@@ -3,12 +3,18 @@
         Job Listings
     </x-slot:heading>
     <h1>
-        @foreach($jobs as $job)
-            <li>
-                <a href="/jobs/{{ $job['id'] }}" class="hover:underline">
-                    <strong>{{ $job['title'] }}</strong>: Pays {{ $job['salary'] }} per year
+        <div class="space-y-4">
+
+            @foreach($jobs as $job)
+            <article class="bg-white shadow-lg rounded-lg overflow-hidden hover:shadow-2xl hover:border-gray-500">
+                <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-200 rounded-lg">
+                    <div class="text-xs font-bold text-blue-500">{{ $job->employer->name }}</div>
+                    <div>
+                        <strong>{{ $job['title'] }}</strong>: Pays {{ $job['salary'] }} per year
+                    </div>
                 </a>
-            </li>
-        @endforeach
+            </article>
+            @endforeach
+        </div>
     </h1>
 </x-layout>
